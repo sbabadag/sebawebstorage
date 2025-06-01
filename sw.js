8b48ff29c4d10dec5adc53f3838479afb5db0d00
sbabadag <cesbabadag@gmail.com>
2025-06-01 16:20:52 +0300

    fix

--- sw.js
+++ sw.js
@@ -8,26 +8,7 @@ const URLS_TO_CACHE = [
   '/projects.html',
   '/blog.html',
   '/contact.html',
-  '/faq.html',
-  '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/css/social-share.css',
-  '/assets/css/newsletter.css',  '/assets/css/responsive.css',
-  '/assets/css/faq.css',
-  '/assets/js/script.js',
-  '/assets/js/translations.js',
-  '/assets/js/language-init.js',
-  '/assets/js/language-enhancer.js', 
-  '/assets/js/language-diagnostic.js',
-  '/assets/js/force-language-update.js',
-  '/assets/js/social-share.js',
-  '/assets/js/newsletter.js',
-  '/assets/images/favicon.ico',
-  '/assets/images/c4cc46a6-6116-4478-a979-e9f52d58f29e.JPEG',
-  '/assets/images/2314f2e2-5780-43fc-b952-8a26e76e4b34.JPEG',
-  '/assets/images/2786a985-ea94-4cd2-83bc-b427783e80ca.JPEG',
-  '/assets/images/36e738dd-a3e4-40b0-bb24-8d332f238fa1.JPEG',
-  '/assets/images/c7e4a198-4281-433f-934b-849b7c907062.JPEG'
+  '/faq.html'
 ];
 
 // Install Service Worker
